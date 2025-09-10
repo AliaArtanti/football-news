ed2d9786f6ee053c82d008abc7bad61e88b7795e
AliaArtanti <al.art295@gmail.com>
2025-09-10 16:31:00 +0700

    tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,11 +33,11 @@ DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alia-artanti-footballnews.pbp.cs.ui.ac.id"]
 
-...
+
 CSRF_TRUSTED_ORIGINS = [
-    "https://alia-artanti-footballnews.pbp.cs.ui.ac.id/"
+    "https://alia-artanti-footballnews.pbp.cs.ui.ac.id"
 ]
-...
+
 
 # Application definition
 
